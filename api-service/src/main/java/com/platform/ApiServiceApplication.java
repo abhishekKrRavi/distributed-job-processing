77722990274d2a5a8920a46029b102582b35b31c
Utkarsh Prakash <utofficial27@gmail.com>
2026-07-24 22:11:23 +0000

    updated multiple files

--- api-service/src/main/java/com/platform/ApiServiceApplication.java
+++ api-service/src/main/java/com/platform/ApiServiceApplication.java
@@ -2,8 +2,10 @@ package com.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.platform")
+@EnableJpaRepositories(basePackages = "com.platform")
 public class ApiServiceApplication {
 
     public static void main(String[] args) {
@@ -11,4 +13,6 @@ public class ApiServiceApplication {
     }
 
 }
+
+
 
